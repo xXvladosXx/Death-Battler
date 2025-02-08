662328cd491056685beeef5a17bc103ecb768f62
Vladyslav <vladyslav.hutsenko@pollub.edu.pl>
2025-02-08 20:49:54 +0100

    Created base hero

---
+++ Assets/CodeBase/Gameplay/Heroes/HeroState.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections.Generic;
+
+namespace CodeBase.Gameplay.Heroes
+{
+  public class HeroState
+  {
+    public float MaxHp;
+    public float CurrentHp;
+    public float Damage;
+    public float Armor;
+    public float CurrentInitiative;
+    public float MaxInitiative;
+
+    public List<SkillState> SkillStates;
+
+    public void ModifyInitiative(float value)
+    {
+      if (CurrentInitiative <= MaxInitiative) 
+        CurrentInitiative += value;
+    }
+  }
+}
